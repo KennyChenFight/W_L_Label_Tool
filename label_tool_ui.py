982fd12f1b96c6d2fa98dc0a537bf889291e7d7a
KennyChen <kennychen851228@gmail.com>
2019-06-13 14:57:55 +0800

    視窗不可放大

--- label_tool_ui.py
+++ label_tool_ui.py
@@ -12,6 +12,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(659, 562)
+        MainWindow.setFixedSize(659, 562)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.pb_half_full_label = QtWidgets.QPushButton(self.centralwidget)
